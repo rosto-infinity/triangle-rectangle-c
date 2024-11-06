53d18c4f5144238095c2a160fd1d4fe524a5992f
ROSTAND LELE <rostoinfocus@gmail.com>
2024-11-06 21:56:23 +0100

    d
    d

--- triangle-rectangle.c
+++ triangle-rectangle.c
@@ -16,28 +16,34 @@
 // Affiche le résultat.
 
 // Fonction pour vérifier si les longueurs peuvent former un triangle rectangle
-int EstTriangleRectangle(double a, double b, double c) {
-    double hypoténuse, côté1, côté2;
+#include <stdio.h>
+#include <math.h>
+
+int estTriangleRectangle(double a, double b, double c) {
+    double hypoténuse, cote1, cote2;
 
     // Identifier l'hypoténuse et les autres côtés
     if (a >= b && a >= c) {
         hypoténuse = a;
-        côté1 = b;
-        côté2 = c;
-    } else if (b >= a && b >= c) {
+        cote1 = b;
+        cote2 = c;
+    }
+    else if (b >= a && b >= c) {
         hypoténuse = b;
-        côté1 = a;
-        côté2 = c;
-    } else {
+        cote1 = a;
+        cote2 = c;
+    }
+    else {
         hypoténuse = c;
-        côté1 = a;
-        côté2 = b;
+        cote1 = a;
+        cote2 = b;
     }
 
     // Vérifier la relation de Pythagore
-    if (hypoténuse * hypoténuse == côté1 * côté1 + côté2 * côté2) {
+    if (hypoténuse * hypoténuse == cote1 * cote1 + cote2 * cote2) {
         return 1; // Vrai
-    } else {
+    }
+    else {
         return 0; // Faux
     }
 }
@@ -46,17 +52,18 @@ int main() {
     double a, b, c;
 
     // Lire les longueurs des côtés
-    printf("Entrez la longueur du côté a : ");
+    printf("Entrez la longueur du cote a : ");
     scanf("%lf", &a);
-    printf("Entrez la longueur du côté b : ");
+    printf("Entrez la longueur du cote b : ");
     scanf("%lf", &b);
-    printf("Entrez la longueur du côté c : ");
+    printf("Entrez la longueur du cote c : ");
     scanf("%lf", &c);
 
     // Vérifier si les longueurs peuvent former un triangle rectangle
-    if (EstTriangleRectangle(a, b, c)) {
+    if (estTriangleRectangle(a, b, c)) {
         printf("Les longueurs peuvent former un triangle rectangle.\n");
-    } else {
+    }
+    else {
         printf("Les longueurs ne peuvent pas former un triangle rectangle.\n");
     }
 
